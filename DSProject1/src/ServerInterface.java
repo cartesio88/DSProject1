e876a38d7fbc5f91778a657ba528a6b48085f87a
RomanRipp <romanripp@x-134-84-47-48.uofm-secure.wireless.umn.edu>
2013-02-06 19:04:59 -0600

    Communicate interface

--- DSProject1/src/ServerInterface.java
+++ DSProject1/src/ServerInterface.java
@@ -1,4 +1,14 @@
 
-public interface ServerInterface {
+package Communicate;
 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Communicate extends Remote {
+    boolean JoinServer() throws RemoteException;
+    boolean Join(string IP, int Port) throws RemoteException;
+    boolean Subscribe(string IP, int Port, string Article) throws RemoteException;
+    boolean Publish(string Article) throws RemoteException;
+    boolean Unsubscribe(string IP, int Port, string Article) throws RemoteException;
+    boolean Ping() throws RemoteException;
 }
