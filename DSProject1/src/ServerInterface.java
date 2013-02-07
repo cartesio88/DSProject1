ded9e7fcc2c9d37691c3881a75e53f3d8fa308e5
Cesar MC <cesarmoracastro@gmail.com>
2013-02-06 19:09:12 -0600

    .

--- DSProject1/src/ServerInterface.java
+++ DSProject1/src/ServerInterface.java
@@ -1,14 +1,12 @@
 
-package Communicate;
-
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface Communicate extends Remote {
+public interface ServerInterface extends Remote {
     boolean JoinServer() throws RemoteException;
-    boolean Join(string IP, int Port) throws RemoteException;
-    boolean Subscribe(string IP, int Port, string Article) throws RemoteException;
-    boolean Publish(string Article) throws RemoteException;
-    boolean Unsubscribe(string IP, int Port, string Article) throws RemoteException;
+    boolean Join(String IP, int Port) throws RemoteException;
+    boolean Subscribe(String IP, int Port, String Article) throws RemoteException;
+    boolean Publish(String Article) throws RemoteException;
+    boolean Unsubscribe(String IP, int Port, String Article) throws RemoteException;
     boolean Ping() throws RemoteException;
 }
