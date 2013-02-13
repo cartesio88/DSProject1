7b0aff2d0fd2b5fd5d65ebc6f7305c59a8f7252d
Cesar MC <cesarmoracastro@gmail.com>
2013-02-13 13:07:47 -0600

    ..

--- DSProject1/src/DSProject/Client.java
+++ DSProject1/src/DSProject/Client.java
@@ -1,5 +1,4 @@
 package DSProject;
-import java.net.InetAddress;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -7,42 +6,27 @@ import java.rmi.registry.Registry;
 
 public class Client implements Constants {
 
-
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
 		System.out.println("Starting the Client");
 
-		/*Start UDP server*/
-
-		UDPServer udpServer = new UDPServer(3333);
-		udpServer.start();
-
-		Thread.sleep(2000);
-
 		try {
-
 			Registry registry = LocateRegistry.getRegistry(serverIp);
 			ServerInterface server = (ServerInterface) registry
 					.lookup(serverName);
 
-			System.out.println("[Client] Pinging");
 			server.Ping();
-			System.out.println("[Client] Joining");
-			server.Join("locahost", 3333);
-			Thread.sleep(2000);
-
-			System.out.println("[Client] Subscribing");
-			server.Subscribe("localhost", 3333,";;;");
-			Thread.sleep(2000);
-
-			System.out.println("[Client] Publishing article");
-			server.Publish(";;;Article1");
-			Thread.sleep(2000);
-
-
-
+			
 		} catch (RemoteException | NotBoundException e) {
 			System.out.println("Error locating the server");
 			System.out.println(e);
 		}
+	}
+}
+
+class UDPClient
+{
+	public static void SendReply() throws Exception
+	{
+		String A;
 	}
 }
