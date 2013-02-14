9f26ac8f34699f9f26699a476c10e943573f017c
Cesar MC <cesarmoracastro@gmail.com>
2013-02-13 20:10:37 -0600

    coo

--- DSProject1/src/DSProject/Client.java
+++ DSProject1/src/DSProject/Client.java
@@ -1,123 +1,128 @@
 package DSProject;
+
 import java.net.InetAddress;
-import java.rmi.AccessException;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Enumeration;
 import java.util.Scanner;
 
 public class Client implements Constants {
 
 	private static int udpPort = 3333;
-	
-	public static void main(String[] args) throws InterruptedException, RemoteException, NotBoundException {
+	private static InetAddress clientIp = null;
+
+	public static void main(String[] args) throws InterruptedException,
+			RemoteException, NotBoundException {
 		System.out.println("Starting the Client");
-		
-		
-		
-		Registry registry = LocateRegistry.getRegistry(serverIp);
-		Communicate server = (Communicate) registry.lookup(serverName);		
-				
-		
-		System.out.println("Choose the option: \n" +
-				"1) Join\n" +
-				"2) Subscribe\n" +
-				"3) Publish\n" +
-				"4) Unsubscribe\n");
-		
-		Scanner scan = new Scanner(System.in);
-		String Choice = scan.nextLine();
-		
-		Integer Option = Integer.valueOf(Choice);
-		
-		String IP = null;
+		System.setProperty("java.net.preferIPv4Stack", "true");
+
+		String serverIp;
 		Integer Port = null;
 		String Article = null;
+		boolean done = false;
 		
-		switch(Option){ 
-		case 1:
-			System.out.println("Enter Server IP: ");
-			IP = scan.nextLine();
-		
-			System.out.println("Enter port: ");
-			Port = Integer.valueOf(scan.nextLine());
-			
-			server.JoinServer(IP, Port);
-			
-			break;
-		case 2:
+		getClientIP();
 
-			System.out.println("Enter Server IP: ");
-			IP = scan.nextLine();
-		
-			System.out.println("Enter port: ");
-			Port = Integer.valueOf(scan.nextLine());
-			
-			System.out.println("Enter Article:");
-			Article = scan.nextLine();
-			
-			server.Subscribe(IP, Port, Article);
-			
-			break;
-			
-		case 3: 
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter Server' IP you want to join: ");
+		serverIp = scan.nextLine();
 
-			System.out.println("Enter Server IP: ");
-			IP = scan.nextLine();
-		
-			System.out.println("Enter port: ");
-			Port = Integer.valueOf(scan.nextLine());
-			
-			System.out.println("Enter Article:");
-			Article = scan.nextLine();
+		System.out.println("Enter port: ");
+		Port = Integer.valueOf(scan.nextLine());
 
-			server.Publish(Article, IP, Port);
-			
-			break;
-			
-		case 4:
-			
-			System.out.println("Enter Server IP: ");
-			IP = scan.nextLine();
-		
-			System.out.println("Enter port: ");
-			Port = Integer.valueOf(scan.nextLine());
-			
-			System.out.println("Enter Article:");
-			Article = scan.nextLine();
-			
-			server.Unsubscribe(IP, Port, Article);
-			
+		Registry registry = LocateRegistry.getRegistry(serverIp);
+		Communicate server = (Communicate) registry.lookup(serverName);
+
+		while (!done) {
+
+			System.out.println("Choose the option: \n" + "1) Join\n"
+					+ "2) Subscribe\n" + "3) Publish\n" + "4) Unsubscribe\n"
+					+ "5) Ping\n" + "6) Exit\n");
+
+			String Choice = scan.nextLine();
+			Integer Option = Integer.valueOf(Choice);
+
+			switch (Option) {
+			case 1:
+				if (server.Join(clientIp.getHostAddress(), udpPort))
+					System.out.println("Joined successfully!");
+				else
+					System.out.println("ERROR joining");
+				break;
+
+			case 2:
+
+				System.out.println("Enter Article:");
+				Article = scan.nextLine();
+				if (server.Subscribe(clientIp.getHostAddress(), udpPort, Article))
+					System.out.println("Subscribed successfully!");
+				else
+					System.out.println("ERROR subscribing");
+				break;
+
+			case 3:
+
+				System.out.println("Enter Article:");
+				Article = scan.nextLine();
+				if (server.Publish(Article, clientIp.getHostAddress(), udpPort)) {
+					System.out.println("Published successfully!");
+				} else {
+					System.out.println("ERROR publishing");
+				}
+				break;
+
+			case 4:
+
+				System.out.println("Enter Article:");
+				Article = scan.nextLine();
+				if (server.Unsubscribe(clientIp.getHostAddress(), udpPort, Article))
+					System.out.println("Unsubscribed successfully!");
+				else
+					System.out.println("ERROR unsubscribing");
+				break;
+			case 5:
+				if (server.Ping()) {
+					System.out.println("Server OK!");
+				} else {
+					System.out.println("Server DOWN!");
+				}
+				break;
+			case 6:
+
+				System.exit(0);
+				break;
+
+			}
 		}
-				
-			
-		/*Start UDP server*/
-		ClientUDPServer udpServer = new ClientUDPServer(udpPort);
-		udpServer.start();
+
+		
+	}
 	
+	
+	private static void getClientIP() {
 		try {
+			Enumeration<NetworkInterface> nets = NetworkInterface
+					.getNetworkInterfaces();
 
-			//Registry registry = LocateRegistry.getRegistry(serverIp);
-			//Communicate server = (Communicate) registry
-			//		.lookup(serverName);
+			while (nets.hasMoreElements()) {
+				NetworkInterface ni = nets.nextElement();
+				if (!ni.isLoopback() && ni.isUp()) {
+					clientIp = ni.getInetAddresses().nextElement();
+					break;
+				}
+			}
+			System.setProperty("java.rmi.server.hostname",clientIp.getHostAddress());
+			
+			System.out.println("El valor de la ip es:"
+					+ System.getProperty("java.rmi.server.hostname"));
 
-			System.out.println("[Client] Pinging");
-			server.Ping();
-			System.out.println("[Client] Joining");
-			server.Join("locahost", udpPort);
-	
-			System.out.println("[Client] Subscribing");
-			server.Subscribe("127.0.0.1", udpPort,";;;");
-
-			System.out.println("[Client] Publishing article");
-			server.Publish(";;;Article1","127.0.0.1",udpPort);
-
-
-
-		} catch (RemoteException e) {
-			System.out.println("Error locating the server");
-			System.out.println(e);
+		} catch (SocketException e) {
+			System.out.println("ERROR getting the interfaces of the device");
+			e.printStackTrace();
 		}
 	}
 }
