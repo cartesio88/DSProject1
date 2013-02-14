43474bcdf72e3befe76019479de00109af53599b
Cesar MC <cesarmoracastro@gmail.com>
2013-02-13 18:22:33 -0600

    Adding policy to server

--- DSProject1/src/Server.java
+++ DSProject1/src/Server.java
@@ -5,6 +5,8 @@ public class Server {
 
 	public static void main(String[] args) throws Exception {
 		System.setProperty("java.net.preferIPv4Stack", "true");
+		System.setProperty("java.rmi.server.codebase", "file:./bin");
+		System.setProperty("java.security.policy", "file:./policyfile");
 		
 		@SuppressWarnings("unused")
 		ServerRMI server = new ServerRMI();	
